7419647b7f3778c27ba4feaa4842d11ec7557c31
SyvashAnatoliy <a.sivash.ps4@gmail.com>
2025-08-26 21:00:49 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+var playList = [
+  {
+    author: "LED ZEPPELIN",
+    song: "STAIRWAY TO HEAVEN"
+  },
+  {
+    author: "QUEEN",
+    song: "BOHEMIAN RHAPSODY"
+  },
+  {
+    author: "LYNYRD SKYNYRD",
+    song: "FREE BIRD"
+  },
+  {
+    author: "DEEP PURPLE",
+    song: "SMOKE ON THE WATER"
+  },
+  {
+    author: "JIMI HENDRIX",
+    song: "ALL ALONG THE WATCHTOWER"
+  },
+  {
+    author: "AC/DC",
+    song: "BACK IN BLACK"
+  },
+  {
+    author: "QUEEN",
+    song: "WE WILL ROCK YOU"
+  },
+  {
+    author: "METALLICA",
+    song: "ENTER SANDMAN"
+  }
+];
+
+var ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+for (var i = 0; i < playList.length; i++) {
+  var li = document.createElement('li');
+  li.innerHTML = playList[i].author + " - " + playList[i].song;
+  ul.appendChild(li);
+}
